161030374f1de2a89d113cc838c4a97d91028c13
chez-shanpu <cheztomo513@gmail.com>
2021-03-15 15:27:18 +0900

    Update srnode agent

--- pkg/srnode/srnode_test.go
+++ pkg/srnode/srnode_test.go
@@ -6,33 +6,35 @@ func Test_calcInterfaceUsagePercent(t *testing.T) {
 	type args struct {
 		firstBytes  int64
 		secondBytes int64
-		firstTime   int
-		secondTime  int
+		duration    float64
 		linkCapBits int64
-		ratioFlag   bool
 	}
 	tests := []struct {
-		name string
-		args args
-		want float64
+		name  string
+		args  args
+		want1 float64
+		want2 float64
 	}{
 		{
 			name: "correct case",
 			args: args{
 				firstBytes:  800,
 				secondBytes: 1600,
-				firstTime:   0,
-				secondTime:  10,
-				linkCapBits: 8000,
-				ratioFlag:   true,
+				duration:    10,
+				linkCapBits: 10000,
 			},
-			want: 8,
+			want1: 6.4,
+			want2: 80,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := calcInterfaceUsage(tt.args.firstBytes, tt.args.secondBytes, float64(tt.args.secondTime-tt.args.firstTime), tt.args.linkCapBits, tt.args.ratioFlag); got != tt.want {
-				t.Errorf("calcInterfaceUsagePercent() = %v, want %v", got, tt.want)
+			got1, got2 := calcInterfaceUsage(tt.args.firstBytes, tt.args.secondBytes, tt.args.duration, tt.args.linkCapBits)
+			if got1 != tt.want1 {
+				t.Errorf("calcInterfaceUsage() got = %v, want %v", got1, tt.want1)
+			}
+			if got2 != tt.want2 {
+				t.Errorf("calcInterfaceUsage() got1 = %v, want %v", got2, tt.want2)
 			}
 		})
 	}
